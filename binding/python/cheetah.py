0203f7dd1e0ae88ac6f46270eb42ea3fe1a4e923
Alireza Kenarsari <alireza@picovoice.ai>
2022-01-18 12:30:15 -0800

    wip

--- binding/python/cheetah.py
+++ binding/python/cheetah.py
@@ -1,5 +1,5 @@
 #
-#    Copyright 2018 Picovoice Inc.
+#    Copyright 2018-2022 Picovoice Inc.
 #
 #    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 #    file accompanying this source.
@@ -11,16 +11,62 @@
 
 import os
 from ctypes import *
-from ctypes.util import find_library
 from enum import Enum
+from typing import *
+
+
+class CheetahError(Exception):
+    pass
+
+
+class CheetahMemoryError(CheetahError):
+    pass
+
+
+class CheetahIOError(CheetahError):
+    pass
+
+
+class CheetahInvalidArgumentError(CheetahError):
+    pass
+
+
+class CheetahStopIterationError(CheetahError):
+    pass
+
+
+class CheetahKeyError(CheetahError):
+    pass
+
+
+class CheetahInvalidStateError(CheetahError):
+    pass
+
+
+class CheetahRuntimeError(CheetahError):
+    pass
+
+
+class CheetahActivationError(CheetahError):
+    pass
+
+
+class CheetahActivationLimitError(CheetahError):
+    pass
+
+
+class CheetahActivationThrottledError(CheetahError):
+    pass
+
+
+class CheetahActivationRefusedError(CheetahError):
+    pass
 
 
 class Cheetah(object):
-    """Python binding for Picovoice's streaming Speech-to-Text engine."""
+    """Python binding for Cheetah streaming speech-to-text engine."""
 
     class PicovoiceStatuses(Enum):
-        """Status codes corresponding to 'pv_status_t' defined in 'include/picovoice.h'"""
-
         SUCCESS = 0
         OUT_OF_MEMORY = 1
         IO_ERROR = 2
@@ -28,14 +74,24 @@ class Cheetah(object):
         STOP_ITERATION = 4
         KEY_ERROR = 5
         INVALID_STATE = 6
+        RUNTIME_ERROR = 7
+        ACTIVATION_ERROR = 8
+        ACTIVATION_LIMIT_REACHED = 9
+        ACTIVATION_THROTTLED = 10
+        ACTIVATION_REFUSED = 11
 
     _PICOVOICE_STATUS_TO_EXCEPTION = {
-        PicovoiceStatuses.OUT_OF_MEMORY: MemoryError,
-        PicovoiceStatuses.IO_ERROR: IOError,
-        PicovoiceStatuses.INVALID_ARGUMENT: ValueError,
-        PicovoiceStatuses.STOP_ITERATION: StopIteration,
-        PicovoiceStatuses.KEY_ERROR: KeyError,
-        PicovoiceStatuses.INVALID_STATE: ValueError,
+        PicovoiceStatuses.OUT_OF_MEMORY: CheetahMemoryError,
+        PicovoiceStatuses.IO_ERROR: CheetahIOError,
+        PicovoiceStatuses.INVALID_ARGUMENT: CheetahInvalidArgumentError,
+        PicovoiceStatuses.STOP_ITERATION: CheetahStopIterationError,
+        PicovoiceStatuses.KEY_ERROR: CheetahKeyError,
+        PicovoiceStatuses.INVALID_STATE: CheetahInvalidStateError,
+        PicovoiceStatuses.RUNTIME_ERROR: CheetahRuntimeError,
+        PicovoiceStatuses.ACTIVATION_ERROR: CheetahActivationError,
+        PicovoiceStatuses.ACTIVATION_LIMIT_REACHED: CheetahActivationLimitError,
+        PicovoiceStatuses.ACTIVATION_THROTTLED: CheetahActivationThrottledError,
+        PicovoiceStatuses.ACTIVATION_REFUSED: CheetahActivationRefusedError
     }
 
     class CCheetah(Structure):
@@ -43,53 +99,48 @@ class Cheetah(object):
 
     def __init__(
             self,
-            library_path,
-            acoustic_model_path,
-            language_model_path,
-            license_path,
-            endpoint_duration_sec=None):
+            access_key: str,
+            library_path: str,
+            model_path: str,
+            endpoint_duration_sec: Optional[float] = None):
         """
-        Loads Cheetah's dynamic library and creates an instance of speech-to-text object.
+        Constructor
 
+        :param access_key: AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)
         :param library_path: Absolute path to Cheetah's dynamic library.
-        :param acoustic_model_path: Absolute path to file containing acoustic model parameters.
-        :param language_model_path: Absolute path to file containing language model parameters.
-        :param license_path : Absolute path to license file.
+        :param model_path: Absolute path to the file containing model parameters.
         :param endpoint_duration_sec Duration of endpoint in seconds. A speech endpoint is detected when there is a
-        chunk of audio (with a duration specified herein) after an utterance without any speech in it. Set to 'None'
+        chunk of audio (with a duration specified herein) after an utterance without any speech in it. Set to `None`
         to disable endpoint detection.
         """
 
-        self._libc = CDLL(find_library('c'))
+        if not isinstance(access_key, str) or len(access_key) == 0:
+            raise CheetahInvalidArgumentError("`access_key` should be a non-empty string.")
 
         if not os.path.exists(library_path):
-            raise IOError("Could not find Cheetah's dynamic library at '%s'" % library_path)
+            raise CheetahIOError("Could not find Cheetah's dynamic library at `%s`." % library_path)
 
         library = cdll.LoadLibrary(library_path)
 
-        if not os.path.exists(acoustic_model_path):
-            raise IOError("Could not find acoustic model file at '%s'" % acoustic_model_path)
+        if not os.path.exists(model_path):
+            raise CheetahIOError("Could not find model file at `%s`." % model_path)
 
-        if not os.path.exists(language_model_path):
-            raise IOError("Could not find language model file at '%s'" % language_model_path)
-
-        if not os.path.exists(license_path):
-            raise IOError("Could not find license file at '%s'" % license_path)
+        if endpoint_duration_sec is not None and not endpoint_duration_sec >= 0.:
+            raise CheetahInvalidArgumentError("`endpoint_duration_sec` is either `None` or a positive number")
 
         init_func = library.pv_cheetah_init
-        init_func.argtypes = [c_char_p, c_char_p, c_char_p, c_int32, POINTER(POINTER(self.CCheetah))]
+        init_func.argtypes = [c_char_p, c_char_p, c_float, POINTER(POINTER(self.CCheetah))]
         init_func.restype = self.PicovoiceStatuses
 
         self._handle = POINTER(self.CCheetah)()
 
         status = init_func(
-            acoustic_model_path.encode(),
-            language_model_path.encode(),
-            license_path.encode(),
-            endpoint_duration_sec if endpoint_duration_sec is not None else -1,
+            access_key.encode(),
+            model_path.encode(),
+            float(endpoint_duration_sec) if endpoint_duration_sec is not None else -1.,
             byref(self._handle))
         if status is not self.PicovoiceStatuses.SUCCESS:
-            raise self._PICOVOICE_STATUS_TO_EXCEPTION[status]('Initialization failed')
+            raise self._PICOVOICE_STATUS_TO_EXCEPTION[status]()
 
         self._delete_func = library.pv_cheetah_delete
         self._delete_func.argtypes = [POINTER(self.CCheetah)]
@@ -108,19 +159,7 @@ class Cheetah(object):
 
         self._frame_length = library.pv_cheetah_frame_length()
 
-    @property
-    def sample_rate(self):
-        """Audio sample rate accepted by Cheetah library."""
-
-        return self._sample_rate
-
-    @property
-    def frame_length(self):
-        """Number of audio samples per frame expected by C library."""
-
-        return self._frame_length
-
-    def process(self, pcm):
+    def process(self, pcm: Sequence[int]) -> Tuple[str, bool]:
         """
         Processes a frame of audio and returns newly-transcribed text (if any) and a flag indicating if an endpoint has
         been detected. Upon detection of an endpoint, the client should invoke 'pv_cheetah_flush()' to retrieve any
@@ -148,11 +187,10 @@ class Cheetah(object):
             raise self._PICOVOICE_STATUS_TO_EXCEPTION[status]('Processing failed')
 
         partial_transcript = c_partial_transcript.value.decode('utf-8')
-        self._libc.free(c_partial_transcript)
 
         return partial_transcript, is_endpoint
 
-    def flush(self):
+    def flush(self) -> str:
         """
         Marks the end of the audio stream, flushes internal state of the object, and returns any remaining transcribed
         text.
@@ -167,11 +205,28 @@ class Cheetah(object):
             raise self._PICOVOICE_STATUS_TO_EXCEPTION[status]('Flush failed')
 
         final_transcript = c_final_transcript.value.decode('utf-8')
-        self._libc.free(c_final_transcript)
 
         return final_transcript
 
-    def delete(self):
-        """Releases resources acquired by Cheetah library."""
+    def delete(self) -> None:
+        """Releases resources acquired by Cheetah."""
 
         self._delete_func(self._handle)
+
+    @property
+    def version(self):
+        """Version."""
+
+        return self._version
+
+    @property
+    def sample_rate(self) -> int:
+        """Audio sample rate accepted by `.process`."""
+
+        return self._sample_rate
+
+    @property
+    def frame_length(self) -> int:
+        """Number of audio samples per frame expected by C library."""
+
+        return self._frame_length
