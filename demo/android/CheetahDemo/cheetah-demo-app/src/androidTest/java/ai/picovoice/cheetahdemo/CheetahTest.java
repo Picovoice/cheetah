5037b65ef59c86e2d7a04bb62aedef962c3a3942
Eric Mikulin <eric@picovoice.ai>
2022-03-30 10:20:41 -0700

    Remove unused import

--- demo/android/CheetahDemo/cheetah-demo-app/src/androidTest/java/ai/picovoice/cheetahdemo/CheetahTest.java
+++ demo/android/CheetahDemo/cheetah-demo-app/src/androidTest/java/ai/picovoice/cheetahdemo/CheetahTest.java
@@ -32,12 +32,11 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.file.Files;
 import java.util.Arrays;
 
 import ai.picovoice.cheetah.Cheetah;
