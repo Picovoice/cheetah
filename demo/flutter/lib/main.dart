b6da4d8ea4ad276f9beea7f7487e811a36a4390f
Ian Lavery <ian@picovoice.ai>
2023-08-09 11:01:36 -0700

    Update demo with new flutter vp v1.1 (#229)

--- demo/flutter/lib/main.dart
+++ demo/flutter/lib/main.dart
@@ -1,5 +1,5 @@
 //
-// Copyright 2022 Picovoice Inc.
+// Copyright 2022-2023 Picovoice Inc.
 //
 // You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 // file accompanying this source.
@@ -113,7 +113,7 @@ class _MyAppState extends State<MyApp> {
         isProcessing = true;
       });
     } on CheetahException catch (ex) {
-      print("Failed to start audio capture: ${ex.message}");
+      errorCallback(ex);
     }
   }
 
@@ -128,7 +128,7 @@ class _MyAppState extends State<MyApp> {
         isProcessing = false;
       });
     } on CheetahException catch (ex) {
-      print("Failed to start audio capture: ${ex.message}");
+      errorCallback(ex);
     }
   }
 
