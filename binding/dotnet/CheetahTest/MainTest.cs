614f3da898e23b0b262fe282de02145c098a1651
Ian <ian@picovoice.ai>
2023-08-16 17:38:12 -0700

    dotnet 2.0 fix

--- binding/dotnet/CheetahTest/MainTest.cs
+++ binding/dotnet/CheetahTest/MainTest.cs
@@ -54,24 +54,27 @@ namespace CheetahTest
         [TestMethod]
         public void TestVersion()
         {
-            using Cheetah cheetah = Cheetah.Create(ACCESS_KEY);
-            Assert.IsFalse(string.IsNullOrWhiteSpace(cheetah?.Version), "Cheetah did not return a valid version number.");
+            using (Cheetah cheetah = Cheetah.Create(ACCESS_KEY)) {
+                Assert.IsFalse(string.IsNullOrWhiteSpace(cheetah?.Version), "Cheetah did not return a valid version number.");
+            }
         }
 
         [TestMethod]
         public void TestSampleRate()
         {
-            using Cheetah cheetah = Cheetah.Create(ACCESS_KEY);
-            int Num = 0;
-            Assert.IsTrue(int.TryParse(cheetah.SampleRate.ToString(), out Num), "Cheetah did not return a valid sample rate.");
+            using (Cheetah cheetah = Cheetah.Create(ACCESS_KEY)) {
+                int num = 0;
+                Assert.IsTrue(int.TryParse(cheetah.SampleRate.ToString(), out num), "Cheetah did not return a valid sample rate.");
+            }
         }
 
         [TestMethod]
         public void TestFrameLength()
         {
-            using Cheetah cheetah = Cheetah.Create(ACCESS_KEY);
-            int Num = 0;
-            Assert.IsTrue(int.TryParse(cheetah.FrameLength.ToString(), out Num), "Cheetah did not return a valid frame length.");
+            using (Cheetah cheetah = Cheetah.Create(ACCESS_KEY)) {
+                int num = 0;
+                Assert.IsTrue(int.TryParse(cheetah.FrameLength.ToString(), out num), "Cheetah did not return a valid frame length.");
+            }
         }
 
         [TestMethod]
@@ -79,30 +82,31 @@ namespace CheetahTest
         [DataRow(false, "Mr quilter is the apostle of the middle classes and we are glad to welcome his gospel")]
         public void TestProcess(bool enableAutomaticPunctuation, string expectedTranscript)
         {
-            using Cheetah cheetah = Cheetah.Create(
+            using (Cheetah cheetah = Cheetah.Create(
                 accessKey: ACCESS_KEY,
                 endpointDurationSec: 0.2f,
-                enableAutomaticPunctuation: enableAutomaticPunctuation);
-            string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples/test.wav");
-            List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
+                enableAutomaticPunctuation: enableAutomaticPunctuation)) {
+                string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples/test.wav");
+                List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
 
-            int frameLen = cheetah.FrameLength;
-            int framecount = (int)Math.Floor((float)(pcm.Count / frameLen));
+                int frameLen = cheetah.FrameLength;
+                int framecount = (int)Math.Floor((float)(pcm.Count / frameLen));
 
-            string transcript = "";
-            bool isEndpoint = false;
-            for (int i = 0; i < framecount; i++)
-            {
-                int start = i * cheetah.FrameLength;
-                List<short> frame = pcm.GetRange(start, frameLen);
-                CheetahTranscript transcriptObj = cheetah.Process(frame.ToArray());
-                transcript += transcriptObj.Transcript;
-                isEndpoint = transcriptObj.IsEndpoint;
+                string transcript = "";
+                bool isEndpoint = false;
+                for (int i = 0; i < framecount; i++)
+                {
+                    int start = i * cheetah.FrameLength;
+                    List<short> frame = pcm.GetRange(start, frameLen);
+                    CheetahTranscript transcriptObj = cheetah.Process(frame.ToArray());
+                    transcript += transcriptObj.Transcript;
+                    isEndpoint = transcriptObj.IsEndpoint;
+                }
+                CheetahTranscript finalTranscriptObj = cheetah.Flush();
+                transcript += finalTranscriptObj.Transcript;
+                Assert.AreEqual(transcript, expectedTranscript);
+                Assert.IsTrue(isEndpoint);
             }
-            CheetahTranscript finalTranscriptObj = cheetah.Flush();
-            transcript += finalTranscriptObj.Transcript;
-            Assert.AreEqual(transcript, expectedTranscript);
-            Assert.IsTrue(isEndpoint);
         }
 
         [TestMethod]
@@ -111,27 +115,28 @@ namespace CheetahTest
         public void TestCustomModel(bool enableAutomaticPunctuation, string expectedTranscript)
         {
             string testModelPath = Path.Combine(_relativeDir, "lib/common/cheetah_params.pv");
-            using Cheetah cheetah = Cheetah.Create(
+            using (Cheetah cheetah = Cheetah.Create(
                 accessKey: ACCESS_KEY,
                 modelPath: testModelPath,
-                enableAutomaticPunctuation: enableAutomaticPunctuation);
-            string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples/test.wav");
-            List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
+                enableAutomaticPunctuation: enableAutomaticPunctuation)) {
+                string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples/test.wav");
+                List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
 
-            int frameLen = cheetah.FrameLength;
-            int framecount = (int)Math.Floor((float)(pcm.Count / frameLen));
+                int frameLen = cheetah.FrameLength;
+                int framecount = (int)Math.Floor((float)(pcm.Count / frameLen));
 
-            string transcript = "";
-            for (int i = 0; i < framecount; i++)
-            {
-                int start = i * cheetah.FrameLength;
-                List<short> frame = pcm.GetRange(start, frameLen);
-                CheetahTranscript transcriptObj = cheetah.Process(frame.ToArray());
-                transcript += transcriptObj.Transcript;
+                string transcript = "";
+                for (int i = 0; i < framecount; i++)
+                {
+                    int start = i * cheetah.FrameLength;
+                    List<short> frame = pcm.GetRange(start, frameLen);
+                    CheetahTranscript transcriptObj = cheetah.Process(frame.ToArray());
+                    transcript += transcriptObj.Transcript;
+                }
+                CheetahTranscript finalTranscriptObj = cheetah.Flush();
+                transcript += finalTranscriptObj.Transcript;
+                Assert.AreEqual(transcript, expectedTranscript);
             }
-            CheetahTranscript finalTranscriptObj = cheetah.Flush();
-            transcript += finalTranscriptObj.Transcript;
-            Assert.AreEqual(transcript, expectedTranscript);
         }
     }
 }
