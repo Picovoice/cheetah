88a83f80847bf4c5ddb1b93198cf9edf9477490c
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-11-07 15:41:34 -0800

    update test

--- binding/rust/tests/cheetah_tests.rs
+++ binding/rust/tests/cheetah_tests.rs
@@ -11,30 +11,63 @@
 
 #[cfg(test)]
 mod tests {
+    use distance::*;
     use itertools::Itertools;
     use rodio::{source::Source, Decoder};
+    use serde_json::Value;
     use std::env;
     use std::fs::File;
     use std::io::BufReader;
 
     use cheetah::CheetahBuilder;
 
-    #[test]
-    fn test_process() {
+    fn load_test_data() -> Value {
+        let test_json: Value = json!([{
+            "language": "en",
+            "transcript": "Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.",
+            "punctuations": ["."],
+            "error_rate": 0.025,
+            "audio_file": "test.wav"
+        }])
+        test_json
+    }
+
+    fn character_error_rate(transcript: &str, expected_transcript: &str) -> f32 {
+        let distance = levenshtein(transcript, expected_transcript);
+        return distance as f32 / expected_transcript.len() as f32;
+    }
+
+    fn run_test_process(
+        language: &str,
+        transcript: &str,
+        punctuations: Vec<&str>,
+        test_punctuation: bool,
+        error_rate: f32,
+        test_audio: &str,
+    ) {
         let access_key = env::var("PV_ACCESS_KEY")
             .expect("Pass the AccessKey in using the PV_ACCESS_KEY env variable");
 
         let audio_path = format!(
-            "{}{}",
+            "{}{}{}",
             env!("CARGO_MANIFEST_DIR"),
-            "/../../resources/audio_samples/test.wav",
+            "/../../resources/audio_samples/",
+            test_audio
         );
+
+        let mut norm_transcript = transcript.to_string();
+        if !test_punctuation {
+            punctuations.iter().for_each(|p| {
+                norm_transcript = norm_transcript.replace(p, "");
+            });
+        }
 
         let audio_file = BufReader::new(File::open(&audio_path).expect(&audio_path));
         let source = Decoder::new(audio_file).unwrap();
 
         let cheetah = CheetahBuilder::new()
             .access_key(access_key)
+            .enable_automatic_punctuation(test_punctuation)
             .init()
             .expect("Unable to create Cheetah");
 
@@ -52,50 +85,65 @@ mod tests {
         let final_transcript = cheetah.flush().unwrap();
         result = format!("{}{}", result, final_transcript.transcript);
 
-        assert_eq!(
-            result,
-            "Mr quilter is the apostle of the middle classes and we are glad to welcome his gospel"
-        )
+        assert!(character_error_rate(&result, &norm_transcript) < error_rate);
+    }
+
+    #[test]
+    fn test_process() {
+        let test_json: Value = load_test_data();
+
+        for t in test_json["tests"]["parameters"].as_array().unwrap() {
+            let language = t["language"].as_str().unwrap();
+            let transcript = t["transcript"].as_str().unwrap();
+            let punctuations = t["punctuations"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|v| v.as_str().unwrap())
+                .collect_vec();
+            let error_rate = t["error_rate"].as_f64().unwrap() as f32;
+
+            let test_audio = t["audio_file"].as_str().unwrap();
+
+            run_test_process(
+                language,
+                transcript,
+                punctuations,
+                false,
+                error_rate,
+                &test_audio,
+            );
+        }
+        Ok(())
     }
 
     #[test]
     fn test_process_punctuation() {
-        let access_key = env::var("PV_ACCESS_KEY")
-            .expect("Pass the AccessKey in using the PV_ACCESS_KEY env variable");
+        let test_json: Value = load_test_data();
 
-        let audio_path = format!(
-            "{}{}",
-            env!("CARGO_MANIFEST_DIR"),
-            "/../../resources/audio_samples/test.wav",
-        );
+        for t in test_json["tests"]["parameters"].as_array().unwrap() {
+            let language = t["language"].as_str().unwrap();
+            let transcript = t["transcript"].as_str().unwrap();
+            let punctuations = t["punctuations"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|v| v.as_str().unwrap())
+                .collect_vec();
+            let error_rate = t["error_rate"].as_f64().unwrap() as f32;
 
-        let audio_file = BufReader::new(File::open(&audio_path).expect(&audio_path));
-        let source = Decoder::new(audio_file).unwrap();
+            let test_audio = t["audio_file"].as_str().unwrap();
 
-        let cheetah = CheetahBuilder::new()
-            .access_key(access_key)
-            .enable_automatic_punctuation(true)
-            .init()
-            .expect("Unable to create Cheetah");
-
-        assert_eq!(cheetah.sample_rate(), source.sample_rate());
-
-        let mut result = String::new();
-        for frame in &source.chunks(cheetah.frame_length() as usize) {
-            let frame = frame.collect_vec();
-            if frame.len() == cheetah.frame_length() as usize {
-                let partial_transcript = cheetah.process(&frame).unwrap();
-                result = format!("{}{}", result, partial_transcript.transcript);
-            }
+            run_test_process(
+                language,
+                transcript,
+                punctuations,
+                true,
+                error_rate,
+                &test_audio,
+            );
         }
-
-        let final_transcript = cheetah.flush().unwrap();
-        result = format!("{}{}", result, final_transcript.transcript);
-
-        assert_eq!(
-            result,
-            "Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel."
-        )
+        Ok(())
     }
 
     #[test]
