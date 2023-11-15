718af1ee0af01f0968fd621c0f95637057de2ab6
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-11-15 14:53:52 -0800

    add set sdk:

--- binding/android/Cheetah/cheetah/src/main/java/ai/picovoice/cheetah/Cheetah.java
+++ binding/android/Cheetah/cheetah/src/main/java/ai/picovoice/cheetah/Cheetah.java
@@ -25,11 +25,17 @@ import java.io.OutputStream;
  */
 public class Cheetah {
 
+    private static String _sdk = "android";
+
     static {
         System.loadLibrary("pv_cheetah");
     }
 
     private long handle;
+
+    public static void setSdk(String sdk) {
+        Cheetah._sdk = sdk;
+    }
 
     /**
      * Constructor.
@@ -48,6 +54,8 @@ public class Cheetah {
             String modelPath,
             float endpointDuration,
             boolean enableAutomaticPunctuation) throws CheetahException {
+        CheetahNative.setSdk(Porcupine._sdk);
+
         handle = CheetahNative.init(
                 accessKey,
                 modelPath,
