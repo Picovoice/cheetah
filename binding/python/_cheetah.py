69550b2cd3c0ee2d86d5403452d5d4d48ee941fe
ben-w-conrad <159072715+ben-w-conrad@users.noreply.github.com>
2024-11-29 13:26:56 -0800

    v2.1 Python (#360)

--- binding/python/_cheetah.py
+++ binding/python/_cheetah.py
@@ -1,12 +1,12 @@
 #
-#    Copyright 2018-2023 Picovoice Inc.
+# Copyright 2018-2024 Picovoice Inc.
 #
-#    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
-#    file accompanying this source.
+# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+# file accompanying this source.
 #
-#    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-#    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-#    specific language governing permissions and limitations under the License.
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 #
 
 import os
@@ -16,7 +16,7 @@ from typing import *
 
 
 class CheetahError(Exception):
-    def __init__(self, message: str = '', message_stack: Sequence[str] = None):
+    def __init__(self, message: str = '', message_stack: Optional[Sequence[str]] = None):
         super().__init__(message)
 
         self._message = message
