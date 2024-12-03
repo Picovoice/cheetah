2c53a7a54e5ec480c9733735020f0afc70676f88
Kwangsoo Yeo <kyeo@picovoice.ai>
2024-12-03 10:26:26 -0800

    v2.1 dotnet (#361)

--- binding/dotnet/CheetahTest/MainTest.cs
+++ binding/dotnet/CheetahTest/MainTest.cs
@@ -12,11 +12,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
-using Fastenshtein;
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json.Linq;
 
 using Pv;
 
@@ -25,9 +26,96 @@ namespace CheetahTest
     [TestClass]
     public class MainTest
     {
-        private static string ACCESS_KEY;
+        private static string _accessKey;
+        private static readonly string ROOT_DIR = Path.Combine(AppContext.BaseDirectory, "../../../../../..");
 
-        private static readonly string _relativeDir = AppContext.BaseDirectory;
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext _)
+        {
+            _accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY");
+        }
+
+        [Serializable]
+        private class LanguageTestJson
+        {
+            public string language { get; set; }
+            public string audio_file { get; set; }
+            public string transcript { get; set; }
+
+            public string[] punctuations { get; set; }
+            public float error_rate { get; set; }
+        }
+
+        private static JObject LoadJsonTestData()
+        {
+            string content = File.ReadAllText(Path.Combine(ROOT_DIR, "resources/.test/test_data.json"));
+            return JObject.Parse(content);
+        }
+
+        private static IEnumerable<object[]> LanguageTestParameters
+        {
+            get
+            {
+                JObject testDataJson = LoadJsonTestData();
+                IList<LanguageTestJson> languageTestJson = ((JArray)testDataJson["tests"]["language_tests"]).ToObject<IList<LanguageTestJson>>();
+                return languageTestJson
+                    .Select(x => new object[] {
+                        x.language,
+                        x.audio_file,
+                        x.transcript,
+                        x.punctuations,
+                        x.error_rate,
+                    });
+            }
+        }
+
+        private static string AppendLanguage(string s, string language)
+        {
+            return language == "en" ? s : $"{s}_{language}";
+        }
+
+        private static int LevenshteinDistance(string[] transcriptWords, string[] referenceWords)
+        {
+            int referenceWordsLen = referenceWords.Length;
+            int transcriptWordsLen = transcriptWords.Length;
+
+            int[,] dp = new int[referenceWordsLen + 1, transcriptWordsLen + 1];
+
+            for (int i = 0; i <= referenceWordsLen; i++) dp[i, 0] = i;
+            for (int j = 0; j <= transcriptWordsLen; j++) dp[0, j] = j;
+
+            for (int i = 1; i <= referenceWordsLen; i++)
+            {
+                for (int j = 1; j <= transcriptWordsLen; j++)
+                {
+                    int cost = referenceWords[i - 1].ToUpper() == transcriptWords[j - 1].ToUpper() ? 0 : 1;
+
+                    dp[i, j] = Math.Min(
+                        Math.Min(dp[i - 1, j] + 1, dp[i, j - 1] + 1),
+                        dp[i - 1, j - 1] + cost
+                    );
+                }
+            }
+
+            return dp[referenceWordsLen, transcriptWordsLen];
+        }
+
+        private static double GetErrorRate(string transcript, string referenceTranscript)
+        {
+            string[] transcriptWords = transcript.Split(' ');
+            string[] referenceTranscriptWords = referenceTranscript.Split(' ');
+
+            int editDistance = LevenshteinDistance(transcriptWords, referenceTranscriptWords);
+            return (double)editDistance / referenceTranscriptWords.Length;
+        }
+
+        private static string GetModelPath(string language)
+        {
+            return Path.Combine(
+                ROOT_DIR,
+                "lib/common",
+                $"{AppendLanguage("cheetah_params", language)}.pv");
+        }
 
         private List<short> GetPcmFromFile(string audioFilePath, int expectedSampleRate)
         {
@@ -47,41 +135,10 @@ namespace CheetahTest
             return data;
         }
 
-        public static IEnumerable<object[]> TestParameters
-        {
-            get
-            {
-                List<object[]> testParameters = new List<object[]>();
-
-                string transcript = "Mr quilter is the apostle of the middle classes and we are glad to welcome his gospel";
-                string transcriptWithPunctuation = "Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.";
-
-                testParameters.Add(new object[]
-                {
-                    "en",
-                    "test.wav",
-                    transcript,
-                    transcriptWithPunctuation,
-                    0.025f
-                });
-
-                return testParameters;
-            }
-        }
-
-        static float GetErrorRate(string transcript, string referenceTranscript)
-            => Levenshtein.Distance(transcript, referenceTranscript) / (float)referenceTranscript.Length;
-
-        [ClassInitialize]
-        public static void ClassInitialize(TestContext _)
-        {
-            ACCESS_KEY = Environment.GetEnvironmentVariable("ACCESS_KEY");
-        }
-
         [TestMethod]
         public void TestVersion()
         {
-            using (Cheetah cheetah = Cheetah.Create(ACCESS_KEY))
+            using (Cheetah cheetah = Cheetah.Create(_accessKey))
             {
                 Assert.IsFalse(string.IsNullOrWhiteSpace(cheetah?.Version), "Cheetah did not return a valid version number.");
             }
@@ -90,7 +147,7 @@ namespace CheetahTest
         [TestMethod]
         public void TestSampleRate()
         {
-            using (Cheetah cheetah = Cheetah.Create(ACCESS_KEY))
+            using (Cheetah cheetah = Cheetah.Create(_accessKey))
             {
                 int num = 0;
                 Assert.IsTrue(int.TryParse(cheetah.SampleRate.ToString(), out num), "Cheetah did not return a valid sample rate.");
@@ -100,7 +157,7 @@ namespace CheetahTest
         [TestMethod]
         public void TestFrameLength()
         {
-            using (Cheetah cheetah = Cheetah.Create(ACCESS_KEY))
+            using (Cheetah cheetah = Cheetah.Create(_accessKey))
             {
                 int num = 0;
                 Assert.IsTrue(int.TryParse(cheetah.FrameLength.ToString(), out num), "Cheetah did not return a valid frame length.");
@@ -108,20 +165,21 @@ namespace CheetahTest
         }
 
         [TestMethod]
-        [DynamicData(nameof(TestParameters))]
+        [DynamicData(nameof(LanguageTestParameters))]
         public void TestProcess(
             string language,
             string testAudioFile,
             string referenceTranscript,
-            string _,
+            string[] punctuations,
             float targetErrorRate)
         {
             using (Cheetah cheetah = Cheetah.Create(
-                accessKey: ACCESS_KEY,
+                accessKey: _accessKey,
+                modelPath: GetModelPath(language),
                 endpointDurationSec: 0.2f,
                 enableAutomaticPunctuation: false))
             {
-                string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples", testAudioFile);
+                string testAudioPath = Path.Combine(ROOT_DIR, "resources/audio_samples", testAudioFile);
                 List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
 
                 int frameLen = cheetah.FrameLength;
@@ -140,25 +198,32 @@ namespace CheetahTest
                 CheetahTranscript finalTranscriptObj = cheetah.Flush();
                 transcript += finalTranscriptObj.Transcript;
 
-                Assert.IsTrue(GetErrorRate(transcript, referenceTranscript) < targetErrorRate);
+                string normalizedTranscript = referenceTranscript;
+                foreach (string punctuation in punctuations)
+                {
+                    normalizedTranscript = normalizedTranscript.Replace(punctuation, "");
+                }
+
+                Assert.IsTrue(GetErrorRate(transcript, normalizedTranscript) <= targetErrorRate);
             }
         }
 
         [TestMethod]
-        [DynamicData(nameof(TestParameters))]
+        [DynamicData(nameof(LanguageTestParameters))]
         public void TestProcessWithPunctuation(
             string language,
             string testAudioFile,
-            string _,
             string referenceTranscript,
+            string[] _,
             float targetErrorRate)
         {
             using (Cheetah cheetah = Cheetah.Create(
-                accessKey: ACCESS_KEY,
+                accessKey: _accessKey,
+                modelPath: GetModelPath(language),
                 endpointDurationSec: 0.2f,
                 enableAutomaticPunctuation: true))
             {
-                string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples", testAudioFile);
+                string testAudioPath = Path.Combine(ROOT_DIR, "resources/audio_samples", testAudioFile);
                 List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
 
                 int frameLen = cheetah.FrameLength;
@@ -177,50 +242,14 @@ namespace CheetahTest
                 CheetahTranscript finalTranscriptObj = cheetah.Flush();
                 transcript += finalTranscriptObj.Transcript;
 
-                Assert.IsTrue(GetErrorRate(transcript, referenceTranscript) < targetErrorRate);
-            }
-        }
-
-        [TestMethod]
-        [DynamicData(nameof(TestParameters))]
-        public void TestCustomModel(
-            string language,
-            string testAudioFile,
-            string referenceTranscript,
-            string _,
-            float targetErrorRate)
-        {
-            string testModelPath = Path.Combine(_relativeDir, "lib/common/cheetah_params.pv");
-            using (Cheetah cheetah = Cheetah.Create(
-                accessKey: ACCESS_KEY,
-                modelPath: testModelPath,
-                enableAutomaticPunctuation: false))
-            {
-                string testAudioPath = Path.Combine(_relativeDir, "resources/audio_samples", testAudioFile);
-                List<short> pcm = GetPcmFromFile(testAudioPath, cheetah.SampleRate);
-
-                int frameLen = cheetah.FrameLength;
-                int framecount = (int)Math.Floor((float)(pcm.Count / frameLen));
-
-                string transcript = "";
-                for (int i = 0; i < framecount; i++)
-                {
-                    int start = i * cheetah.FrameLength;
-                    List<short> frame = pcm.GetRange(start, frameLen);
-                    CheetahTranscript transcriptObj = cheetah.Process(frame.ToArray());
-                    transcript += transcriptObj.Transcript;
-                }
-                CheetahTranscript finalTranscriptObj = cheetah.Flush();
-                transcript += finalTranscriptObj.Transcript;
-
-                Assert.IsTrue(GetErrorRate(transcript, referenceTranscript) < targetErrorRate);
+                Assert.IsTrue(GetErrorRate(transcript, referenceTranscript) <= targetErrorRate);
             }
         }
 
         [TestMethod]
         public void TestMessageStack()
         {
-            string modelPath = Path.Combine(_relativeDir, "lib/common/cheetah_params.pv");
+            string modelPath = GetModelPath("en");
 
             Cheetah c;
             string[] messageList = new string[] { };
@@ -263,10 +292,10 @@ namespace CheetahTest
         [TestMethod]
         public void TestProcessFlushMessageStack()
         {
-            string modelPath = Path.Combine(_relativeDir, "lib/common/cheetah_params.pv");
+            string modelPath = GetModelPath("en");
 
             Cheetah c = Cheetah.Create(
-                accessKey: ACCESS_KEY,
+                accessKey: _accessKey,
                 modelPath: modelPath,
                 enableAutomaticPunctuation: false);
             short[] testPcm = new short[c.FrameLength];
