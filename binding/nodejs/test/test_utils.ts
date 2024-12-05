edfbd57d41eef5e74f45adc0d012dff986cd39bb
Kwangsoo Yeo <kyeo@picovoice.ai>
2024-12-05 14:13:49 -0800

    v2.1 node

--- binding/nodejs/test/test_utils.ts
+++ binding/nodejs/test/test_utils.ts
@@ -1,5 +1,5 @@
 //
-// Copyright 2023 Picovoice Inc.
+// Copyright 2024 Picovoice Inc.
 //
 // You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 // file accompanying this source.
@@ -8,19 +8,27 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 //
+import * as fs from 'fs';
 import * as path from 'path';
 
 const ROOT_DIR = path.join(__dirname, '../../..');
+const TEST_DATA_JSON = require(path.join(
+  ROOT_DIR,
+  'resources/.test/test_data.json'
+));
+const MB_50 = 1024 * 1024 * 50;
 
-export const TRANSCRIPT =
-  'Mr quilter is the apostle of the middle classes and we are glad to welcome his gospel';
-export const TRANSCRIPT_WITH_PUNCTUATION =
-    'Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.';
+function appendLanguage(s: string, language: string): string {
+  if (language === 'en') {
+    return s;
+  }
+  return s + '_' + language;
+}
 
-export function getModelPath(): string {
+export function getModelPathByLanguage(language: string): string {
   return path.join(
     ROOT_DIR,
-    `lib/common/cheetah_params.pv`
+    `${appendLanguage('lib/common/cheetah_params', language)}.pv`
   );
 }
 
@@ -28,14 +36,43 @@ export function getAudioFile(audioFile: string): string {
   return path.join(ROOT_DIR, 'resources/audio_samples', audioFile);
 }
 
-export function getTestParameters(): [
+function getCpuPart(): string {
+  if (!fs.existsSync('/proc/cpuinfo')) {
+    return "";
+  }
+  const cpuInfo = fs.readFileSync('/proc/cpuinfo', 'ascii');
+  for (const infoLine of cpuInfo.split('\n')) {
+    if (infoLine.includes('CPU part')) {
+      const infoLineSplit = infoLine.split(' ');
+      return infoLineSplit[infoLineSplit.length - 1].toLowerCase();
+    }
+  }
+  return "";
+}
+
+function getModelSize(language: string): number {
+  const modelPath = getModelPathByLanguage(language);
+  const stats = fs.statSync(modelPath);
+  return stats.size;
+}
+
+export function getLanguageTestParameters(): [
   string,
   string,
   string,
+  string[],
   number,
-  string
 ][] {
-  return [
-    ["en", TRANSCRIPT, TRANSCRIPT_WITH_PUNCTUATION, 0.025, "test.wav"]
-  ];
+  const cpuPart = getCpuPart();
+  let parametersJson = TEST_DATA_JSON.tests.language_tests;
+  if (cpuPart === "0xd03") {
+    parametersJson = parametersJson.filter((x: any) => (getModelSize(x.language) < MB_50));
+  }
+  return parametersJson.map((x: any) => [
+    x.language,
+    x.audio_file,
+    x.transcript,
+    x.punctuations,
+    x.error_rate,
+  ]);
 }
