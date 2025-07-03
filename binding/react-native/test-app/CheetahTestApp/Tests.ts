8063ac2bbf5ca77202557146c2b0b2a7debdd157
Albert Ho <albert@picovoice.ai>
2025-07-02 19:20:37 -0700

    fix test wer

--- binding/react-native/test-app/CheetahTestApp/Tests.ts
+++ binding/react-native/test-app/CheetahTestApp/Tests.ts
@@ -41,7 +41,7 @@ const levenshteinDistance = (words1: string[], words2: string[]) => {
 
 const wordErrorRate = (reference: string, hypothesis: string): number => {
   const ed = levenshteinDistance(reference.split(' '), hypothesis.split(' '));
-  return ed / reference.length;
+  return ed / reference.split(' ').length;
 };
 
 function logResult(result: Result) {
