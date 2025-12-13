655bcc8e37e64ed41d540f29bc2ade89fa9b5a7f
Zhuojin Liu <liu@picovoice.ai>
2025-12-12 17:28:26 -0800

    wip

--- binding/web/src/cheetah.ts
+++ binding/web/src/cheetah.ts
@@ -20,8 +20,8 @@ import {
   loadModel,
 } from '@picovoice/web-utils';
 
-import createModule from "./lib/pv_cheetah";
 import createModuleSimd from "./lib/pv_cheetah_simd";
+import createModulePThread from "./lib/pv_cheetah_pthread";
 
 import { simd } from 'wasm-feature-detect';
 
@@ -33,9 +33,15 @@ import { pvStatusToException } from './cheetah_errors';
 /**
  * WebAssembly function types
  */
-type pv_cheetah_init_type = (accessKey: number, modelPath: number, endpointDurationSec: number, enableAutomaticPunctuation: number, object: number) => Promise<number>;
-type pv_cheetah_process_type = (object: number, pcm: number, transcript: number, isEndpoint: number) => Promise<number>;
-type pv_cheetah_flush_type = (object: number, transcript: number) => Promise<number>;
+type pv_cheetah_init_type = (
+  accessKey: number,
+  modelPath: number,
+  device: number,
+  endpointDurationSec: number,
+  enableAutomaticPunctuation: number,
+  object: number) => number;
+type pv_cheetah_process_type = (object: number, pcm: number, transcript: number, isEndpoint: number) => number;
+type pv_cheetah_flush_type = (object: number, transcript: number) => number;
 type pv_cheetah_transcript_delete_type = (transcript: number) => void;
 type pv_cheetah_delete_type = (object: number) => void;
 type pv_cheetah_frame_length_type = () => number;
@@ -44,6 +50,14 @@ type pv_cheetah_version_type = () => number;
 type pv_set_sdk_type = (sdk: number) => void;
 type pv_get_error_stack_type = (messageStack: number, messageStackDepth: number) => number;
 type pv_free_error_stack_type = (messageStack: number) => void;
+type pv_cheetah_list_hardware_devices_type = (
+  hardwareDevices: number,
+  numHardwareDevices: number
+) => number;
+type pv_cheetah_free_hardware_devices_type = (
+  hardwareDevices: number,
+  numHardwareDevices: number
+) => number;
 
 /**
  * JavaScript/WebAssembly Binding for Cheetah
@@ -89,7 +103,7 @@ type CheetahWasmOutput = {
 const PV_STATUS_SUCCESS = 10000;
 
 export class Cheetah {
-  private readonly _module: CheetahModule;
+  private _module?: CheetahModule;
 
   private readonly _pv_cheetah_process: pv_cheetah_process_type;
   private readonly _pv_cheetah_flush: pv_cheetah_flush_type;
@@ -107,10 +121,10 @@ export class Cheetah {
   private readonly _messageStackAddressAddressAddress: number;
   private readonly _messageStackDepthAddress: number;
 
-  private static _wasm: string;
-  private static _wasmLib: string;
   private static _wasmSimd: string;
   private static _wasmSimdLib: string;
+  private static _wasmPThread: string;
+  private static _wasmPThreadLib: string;
   private static _sdk: string = "web";
 
   private static _cheetahMutex = new Mutex();
@@ -167,26 +181,6 @@ export class Cheetah {
   }
 
   /**
-   * Set base64 wasm file.
-   * @param wasm Base64'd wasm file to use to initialize wasm.
-   */
-  public static setWasm(wasm: string): void {
-    if (this._wasm === undefined) {
-      this._wasm = wasm;
-    }
-  }
-
-  /**
-   * Set base64 wasm file in text format.
-   * @param wasmLib Base64'd wasm file in text format.
-   */
-  public static setWasmLib(wasmLib: string): void {
-    if (this._wasmLib === undefined) {
-      this._wasmLib = wasmLib;
-    }
-  }
-
-  /**
    * Set base64 wasm file with SIMD feature.
    * @param wasmSimd Base64'd wasm file to use to initialize wasm.
    */
@@ -203,6 +197,26 @@ export class Cheetah {
   public static setWasmSimdLib(wasmSimdLib: string): void {
     if (this._wasmSimdLib === undefined) {
       this._wasmSimdLib = wasmSimdLib;
+    }
+  }
+
+  /**
+   * Set base64 wasm file with SIMD and pthread feature.
+   * @param wasmPThread Base64'd wasm file to use to initialize wasm.
+   */
+  public static setWasmPThread(wasmPThread: string): void {
+    if (this._wasmPThread === undefined) {
+      this._wasmPThread = wasmPThread;
+    }
+  }
+
+  /**
+   * Set base64 SIMD and thread wasm file in text format.
+   * @param wasmPThreadLib Base64'd wasm file in text format.
+   */
+  public static setWasmPThreadLib(wasmPThreadLib: string): void {
+    if (this._wasmPThreadLib === undefined) {
+      this._wasmPThreadLib = wasmPThreadLib;
     }
   }
 
@@ -225,6 +239,12 @@ export class Cheetah {
    * @param model.forceWrite Flag to overwrite the model in storage even if it exists.
    * @param model.version Version of the model file. Increment to update the model file in storage.
    * @param options Optional configuration arguments.
+   * @param options.device String representation of the device (e.g., CPU or GPU) to use. If set to `best`, the most
+   * suitable device is selected automatically. If set to `gpu`, the engine uses the first available GPU device. To
+   * select a specific GPU device, set this argument to `gpu:${GPU_INDEX}`, where `${GPU_INDEX}` is the index of the
+   * target GPU. If set to `cpu`, the engine will run on the CPU with the default number of threads. To specify the
+   * number of threads, set this argument to `cpu:${NUM_THREADS}`, where `${NUM_THREADS}` is the desired number of
+   * threads.
    * @param options.endpointDurationSec Duration of endpoint in seconds. A speech endpoint is detected when there is a
    * chunk of audio (with a duration specified herein) after an utterance without any speech in it. Set to `0`
    * to disable endpoint detection.
@@ -258,21 +278,42 @@ export class Cheetah {
     options: CheetahOptions = {},
   ): Promise<Cheetah> {
     const { processErrorCallback } = options;
+    let { device = "best" } = options;
 
     if (!isAccessKeyValid(accessKey)) {
       throw new CheetahErrors.CheetahInvalidArgumentError('Invalid AccessKey');
     }
 
+    const isSimd = await simd();
+    if (!isSimd) {
+      throw new CheetahErrors.CheetahRuntimeError('Browser not supported.');
+    }
+
+    const isWorkerScope =
+      typeof WorkerGlobalScope !== 'undefined' &&
+      self instanceof WorkerGlobalScope;
+    if (
+      !isWorkerScope &&
+      (device === 'best' || (device.startsWith('cpu') && device !== 'cpu:1'))
+    ) {
+      // eslint-disable-next-line no-console
+      console.warn('Multi-threading is not supported on main thread.');
+      device = 'cpu:1';
+    }
+
+    const sabDefined = typeof SharedArrayBuffer !== 'undefined'
+      && (device !== "cpu:1");
+
     return new Promise<Cheetah>((resolve, reject) => {
       Cheetah._cheetahMutex
         .runExclusive(async () => {
-          const isSimd = await simd();
           const wasmOutput = await Cheetah.initWasm(
             accessKey.trim(),
             modelPath.trim(),
-            (isSimd) ? this._wasmSimd : this._wasm,
-            (isSimd) ? this._wasmSimdLib : this._wasmLib,
-            (isSimd) ? createModuleSimd : createModule,
+            device,
+            (sabDefined) ? this._wasmPThread : this._wasmSimd,
+            (sabDefined) ? this._wasmPThreadLib : this._wasmSimdLib,
+            (sabDefined) ? createModulePThread : createModuleSimd,
             options);
           return new Cheetah(wasmOutput, transcriptCallback, processErrorCallback);
         })
@@ -464,6 +505,7 @@ export class Cheetah {
   private static async initWasm(
     accessKey: string,
     modelPath: string,
+    device: string,
     wasmBase64: string,
     wasmLibBase64: string,
     createModuleFunc: any,
@@ -487,7 +529,7 @@ export class Cheetah {
     const pv_cheetah_init: pv_cheetah_init_type = this.wrapAsyncFunction(
       module,
       "pv_cheetah_init",
-      5);
+      6);
     const pv_cheetah_process: pv_cheetah_process_type = this.wrapAsyncFunction(
       module,
       "pv_cheetah_process",
@@ -532,6 +574,16 @@ export class Cheetah {
     module.HEAPU8.set(modelPathEncoded, modelPathAddress);
     module.HEAPU8[modelPathAddress + modelPathEncoded.length] = 0;
 
+    const deviceEncoded = new TextEncoder().encode(device);
+    const deviceAddress = module._malloc((device.length + 1) * Uint8Array.BYTES_PER_ELEMENT);
+    if (deviceAddress === 0) {
+      throw new CheetahErrors.CheetahOutOfMemoryError(
+        'malloc failed: Cannot allocate memory'
+      );
+    }
+    module.HEAP8.set(deviceEncoded, deviceAddress);
+    module.HEAPU8[deviceAddress + deviceEncoded.length] = 0;
+
     const sdkEncoded = new TextEncoder().encode(this._sdk);
     const sdkAddress = module._malloc((sdkEncoded.length + 1) * Uint8Array.BYTES_PER_ELEMENT);
     if (!sdkAddress) {
@@ -555,11 +607,13 @@ export class Cheetah {
     const status = await pv_cheetah_init(
       accessKeyAddress,
       modelPathAddress,
+      deviceAddress,
       endpointDurationSec,
       (enableAutomaticPunctuation) ? 1 : 0,
       objectAddressAddress);
     module._pv_free(accessKeyAddress);
     module._pv_free(modelPathAddress);
+    module._pv_free(deviceAddress);
 
     if (status !== PV_STATUS_SUCCESS) {
       const messageStack = Cheetah.getMessageStack(
@@ -571,7 +625,7 @@ export class Cheetah {
         module.HEAPU8,
       );
 
-      throw pvStatusToException(status, "Initialization failed", messageStack);
+      throw pvStatusToException(status, "Initialization failed");
     }
 
     const objectAddress = module.HEAP32[objectAddressAddress / Int32Array.BYTES_PER_ELEMENT];
@@ -607,6 +661,111 @@ export class Cheetah {
       messageStackAddressAddressAddress: messageStackAddressAddressAddress,
       messageStackDepthAddress: messageStackDepthAddress,
     };
+  }
+
+  /**
+   * Lists all available devices that Cheetah can use for inference.
+   * Each entry in the list can be the used as the `device` argument for the `.create` method.
+   *
+   * @returns List of all available devices that Cheetah can use for inference.
+   */
+  public static async listAvailableDevices(): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+      Cheetah._cheetahMutex
+        .runExclusive(async () => {
+          const isSimd = await simd();
+          if (!isSimd) {
+            throw new CheetahErrors.CheetahRuntimeError('Unsupported Browser');
+          }
+
+          const blob = new Blob(
+            [base64ToUint8Array(this._wasmSimdLib)],
+            { type: 'application/javascript' }
+          );
+          const module: CheetahModule = await createModuleSimd({
+            mainScriptUrlOrBlob: blob,
+            wasmBinary: base64ToUint8Array(this._wasmSimd),
+          });
+
+          const hardwareDevicesAddressAddress = module._malloc(Int32Array.BYTES_PER_ELEMENT);
+          if (hardwareDevicesAddressAddress === 0) {
+            throw new CheetahErrors.CheetahOutOfMemoryError(
+              'malloc failed: Cannot allocate memory for hardwareDevices'
+            );
+          }
+
+          const numHardwareDevicesAddress = module._malloc(Int32Array.BYTES_PER_ELEMENT);
+          if (numHardwareDevicesAddress === 0) {
+            throw new CheetahErrors.CheetahOutOfMemoryError(
+              'malloc failed: Cannot allocate memory for numHardwareDevices'
+            );
+          }
+
+          const status: PvStatus = module._pv_cheetah_list_hardware_devices(
+            hardwareDevicesAddressAddress,
+            numHardwareDevicesAddress
+          );
+
+          const messageStackDepthAddress = module._malloc(Int32Array.BYTES_PER_ELEMENT);
+          if (!messageStackDepthAddress) {
+            throw new CheetahErrors.CheetahOutOfMemoryError(
+              'malloc failed: Cannot allocate memory for messageStackDepth'
+            );
+          }
+
+          const messageStackAddressAddressAddress = module._malloc(Int32Array.BYTES_PER_ELEMENT);
+          if (!messageStackAddressAddressAddress) {
+            throw new CheetahErrors.CheetahOutOfMemoryError(
+              'malloc failed: Cannot allocate memory messageStack'
+            );
+          }
+
+          if (status !== PvStatus.SUCCESS) {
+            const messageStack = await Cheetah.getMessageStack(
+              module._pv_get_error_stack,
+              module._pv_free_error_stack,
+              messageStackAddressAddressAddress,
+              messageStackDepthAddress,
+              module.HEAP32,
+              module.HEAPU8,
+            );
+            module._pv_free(messageStackAddressAddressAddress);
+            module._pv_free(messageStackDepthAddress);
+
+            throw pvStatusToException(
+              status,
+              'List devices failed',
+              messageStack
+            );
+          }
+          module._pv_free(messageStackAddressAddressAddress);
+          module._pv_free(messageStackDepthAddress);
+
+          const numHardwareDevices: number = module.HEAP32[numHardwareDevicesAddress / Int32Array.BYTES_PER_ELEMENT];
+          module._pv_free(numHardwareDevicesAddress);
+
+          const hardwareDevicesAddress = module.HEAP32[hardwareDevicesAddressAddress / Int32Array.BYTES_PER_ELEMENT];
+
+          const hardwareDevices: string[] = [];
+          for (let i = 0; i < numHardwareDevices; i++) {
+            const deviceAddress = module.HEAP32[hardwareDevicesAddress / Int32Array.BYTES_PER_ELEMENT + i];
+            hardwareDevices.push(arrayBufferToStringAtIndex(module.HEAPU8, deviceAddress));
+          }
+          module._pv_cheetah_free_hardware_devices(
+            hardwareDevicesAddress,
+            numHardwareDevices
+          );
+          module._pv_free(hardwareDevicesAddressAddress);
+
+          return hardwareDevices;
+        })
+        .then((result: string[]) => {
+          resolve(result);
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
   }
 
   private static getMessageStack(
