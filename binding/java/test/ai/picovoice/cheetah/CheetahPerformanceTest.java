40e9ca03679afcce80bfdec5bb0c98272f5f419e
picoliu <liu@picovoice.ai>
2025-12-16 13:23:57 -0800

    v3.0 java (#492)

--- binding/java/test/ai/picovoice/cheetah/CheetahPerformanceTest.java
+++ binding/java/test/ai/picovoice/cheetah/CheetahPerformanceTest.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2022-2023 Picovoice Inc.
+    Copyright 2022-2025 Picovoice Inc.
 
     You may not use this file except in compliance with the license. A copy of the license is
     located in the "LICENSE" file accompanying this source.
@@ -26,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CheetahPerformanceTest {
     private final String accessKey = System.getProperty("pvTestingAccessKey");
+    private final String device = System.getProperty("pvTestingDevice");
     private final int numTestIterations = Integer.parseInt(System.getProperty("numTestIterations"));
     private final double initPerformanceThresholdSec =
             Double.parseDouble(System.getProperty("initPerformanceThresholdSec"));
@@ -41,6 +42,7 @@ public class CheetahPerformanceTest {
             Cheetah cheetah = new Cheetah.Builder()
                     .setAccessKey(accessKey)
                     .setModelPath(Utils.getPackagedModelPath())
+                    .setDevice(device)
                     .setLibraryPath(Utils.getPackagedLibraryPath())
                     .build();
 
