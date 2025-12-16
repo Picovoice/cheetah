6c060bad8f63ada27daac453afe5b5c395c64b51
Zhuojin Liu <liu@picovoice.ai>
2025-12-15 16:10:20 -0800

    fix

--- binding/java/src/ai/picovoice/cheetah/Cheetah.java
+++ binding/java/src/ai/picovoice/cheetah/Cheetah.java
@@ -41,13 +41,13 @@ public class Cheetah {
      *
      * @param accessKey                  AccessKey obtained from Picovoice Console.
      * @param modelPath                  Absolute path to the file containing model parameters.
-     * @param device                     String representation of the device (e.g., CPU or GPU) to use. If set to `best`, the most
-     *                                   suitable device is selected automatically. If set to `gpu`, the engine uses the first
-     *                                   available GPU device. To select a specific GPU device, set this argument to
-     *                                   `gpu:${GPU_INDEX}`, where `${GPU_INDEX}` is the index of the target GPU. If set to `cpu`,
-     *                                   the engine will run on the CPU with the default number of threads. To specify the number
-     *                                   of threads, set this argument to `cpu:${NUM_THREADS}`, where `${NUM_THREADS}`
-     *                                   is the desired number of threads.
+     * @param device                     String representation of the device (e.g., CPU or GPU) to use. If set to `best`, 
+     *                                   the most suitable device is selected automatically. If set to `gpu`, the engine
+     *                                   uses the first available GPU device. To select a specific GPU device, set this
+     *                                   argument to `gpu:${GPU_INDEX}`, where `${GPU_INDEX}` is the index of the target
+     *                                   GPU. If set to `cpu`, the engine will run on the CPU with the default number of
+     *                                   threads. To specify the number of threads, set this argument to
+     *                                   `cpu:${NUM_THREADS}`, where `${NUM_THREADS}` is the desired number of threads.
      * @param libraryPath                Absolute path to the native Cheetah library.
      * @param endpointDurationSec        Duration of endpoint in seconds. A speech endpoint is detected when there is a
      *                                   chunk of audio (with a duration specified herein) after an utterance without
