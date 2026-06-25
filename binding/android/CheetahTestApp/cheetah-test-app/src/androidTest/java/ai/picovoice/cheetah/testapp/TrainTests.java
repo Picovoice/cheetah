f898a01c2b0a8ffa39dde40ae30a43941b432838
Eric Mikulin <ErisMik@users.noreply.github.com>
2026-06-25 16:20:59 -0700

    Cheetah api android (#680)
    * Initial android api
    
    * Linting
    
    * Update test
    
    * Docs cleanup
    
    * Address PR comments
    
    * Fix improper serialization
    
    * Appease linter
    
    * Android Release
    
    * Android Release

---
+++ binding/android/CheetahTestApp/cheetah-test-app/src/androidTest/java/ai/picovoice/cheetah/testapp/TrainTests.java
@@ -0,0 +1,62 @@
+/*
+    Copyright 2026 Picovoice Inc.
+
+    You may not use this file except in compliance with the license. A copy of the license is
+    located in the "LICENSE" file accompanying this source.
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+    express or implied. See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package ai.picovoice.cheetah.testapp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ai.picovoice.cheetah.Cheetah;
+import ai.picovoice.cheetah.CheetahException;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class TrainTests extends BaseTest {
+
+    @Test
+    public void testTrainModel() throws CheetahException, IOException {
+        String outputPath = appContext.getFileStreamPath("custom_cheetah_params.pv").getAbsolutePath();
+
+        Map<String, Set<String>> newWords = new HashMap<>();
+        newWords.put("picovoice", new HashSet<>(Arrays.asList("t l k dʒ ɛ dʒ")));
+
+        Cheetah.trainModelFromWords(
+                accessKey,
+                outputPath,
+                "en",
+                newWords,
+                new HashSet<>(Arrays.asList("computer"))
+        );
+
+        Cheetah c = new Cheetah.Builder()
+                .setAccessKey(accessKey)
+                .setDevice(device)
+                .setModelPath(outputPath)
+                .build(appContext);
+
+        assertTrue(c.getVersion() != null && !c.getVersion().equals(""));
+        assertTrue(c.getFrameLength() > 0);
+        assertTrue(c.getSampleRate() > 0);
+
+        c.delete();
+    }
+}
