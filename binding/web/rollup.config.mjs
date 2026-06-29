1bdb8f969cefd54ac9b4962eb8e6bf6e29403544
Matthew Maxwell <matthew@picovoice.ai>
2026-06-29 13:40:44 -0700

    fix

--- binding/web/rollup.config.mjs
+++ binding/web/rollup.config.mjs
@@ -1,14 +1,18 @@
 'use strict';
-import path from 'path';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import workerLoader from 'rollup-plugin-web-worker-loader';
-import pkg from './package.json' with { type: "json" };
-import { babel } from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
-import { DEFAULT_EXTENSIONS } from '@babel/core';
+import path from 'path'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import workerLoader from 'rollup-plugin-web-worker-loader'
+import pkg from './package.json' with { type: 'json' }
+import { babel } from '@rollup/plugin-babel'
+import terser from '@rollup/plugin-terser'
+import { DEFAULT_EXTENSIONS } from '@babel/core'
 import { base64 } from '@picovoice/web-utils/plugins/index.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const extensions = [...DEFAULT_EXTENSIONS, '.ts'];
 
@@ -27,27 +31,27 @@ const iifeBundleName = pkg.name
 console.log(iifeBundleName);
 
 export default {
-  input: [path.resolve(import.meta.dirname, pkg.entry)],
+  input: [path.resolve(__dirname, pkg.entry)],
   output: [
     {
-      file: path.resolve(import.meta.dirname, pkg.module),
+      file: path.resolve(__dirname, pkg['module']),
       format: 'esm',
       sourcemap: false,
     },
     {
-      file: path.resolve(import.meta.dirname, 'dist', 'esm', 'index.min.js'),
+      file: path.resolve(__dirname, 'dist', 'esm', 'index.min.js'),
       format: 'esm',
       sourcemap: false,
       plugins: [terser()],
     },
     {
-      file: path.resolve(import.meta.dirname, pkg.iife),
+      file: path.resolve(__dirname, pkg.iife),
       format: 'iife',
       name: iifeBundleName,
       sourcemap: false,
     },
     {
-      file: path.resolve(import.meta.dirname, 'dist', 'iife', 'index.min.js'),
+      file: path.resolve(__dirname, 'dist', 'iife', 'index.min.js'),
       format: 'iife',
       name: iifeBundleName,
       sourcemap: false,
@@ -59,7 +63,7 @@ export default {
     commonjs(),
     workerLoader({ targetPlatform: 'browser', sourcemap: false }),
     typescript({
-      cacheRoot: path.resolve(import.meta.dirname, '.rts2_cache'),
+      cacheRoot: path.resolve(__dirname, '.rts2_cache'),
       clean: true,
     }),
     babel({
